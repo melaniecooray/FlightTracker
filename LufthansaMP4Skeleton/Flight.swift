56ae963506945e0a3642b2bb3e71ae94e0da6ccd
Max Miranda <maxmiranda@berkeley.edu>
2019-03-02 01:14:06 -0800

    first commit

---
+++ LufthansaMP4Skeleton/Flight.swift
@@ -0,0 +1,13 @@
+//
+//  Flight.swift
+//  LufthansaMP4Skeleton
+//
+//  Created by Max Miranda on 3/2/19.
+//  Copyright © 2019 ___MaxAMiranda___. All rights reserved.
+//
+
+import Foundation
+
+class Flight {
+    //FIXME
+}
